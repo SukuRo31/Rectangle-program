b747ce9a913d81d5d0a86cb2a2a6ed85a2d57bf7
sukumar <suku310598@gmail.com>
2021-11-10 16:21:23 +0530

    my new

---
+++ Rectangle.java
@@ -0,0 +1,13 @@
+public class Rectangle
+{
+public static void main(String[] args)
+{
+Rectangle rectangle = new Rectangle();
+int k = rectangle.calculateArea(10,20);
+System.out.println(k);
+}
+public int calculateArea(int l,int b)
+{
+return l*b;
+}
+}
