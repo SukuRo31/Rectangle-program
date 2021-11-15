3fd34d18ff0c8157dada67dc0bd7f14c04528140
sukumar <suku310598@gmail.com>
2021-11-15 11:38:33 +0530

    package

---
+++ Office.java
@@ -0,0 +1,20 @@
+package tamilnadu.chennai;
+public class Office
+{
+int ta = 100;
+public static void main(String[] args)
+{
+Office employee1 = new Office();
+System.out.println("check");
+employee1.giveSnacks();
+employee1.work();
+}
+void giveSnacks()
+{
+System.out.println("Snacks");
+}
+public void work()
+{
+System.out.println("work");
+}
+}
