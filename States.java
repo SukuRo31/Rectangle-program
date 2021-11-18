a3a9e20fb0fa5d49d83a4d4602f740d20d32f134
sukumar <suku310598@gmail.com>
2021-11-18 16:09:57 +0530

    my package

---
+++ States.java
@@ -0,0 +1,16 @@
+package tamilpeople;
+public class States
+{
+public int cash=100000;
+public static void main(String[] args)
+{
+
+
+}
+public void funds()
+{
+System.out.println("50 cr");
+}
+
+} 
+
