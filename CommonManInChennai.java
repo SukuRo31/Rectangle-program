69762d8b9fbb3f086332faf5ec35569bcd868279
sukumar <suku310598@gmail.com>
2021-11-29 11:06:35 +0530

    scenario

---
+++ CommonManInChennai.java
@@ -0,0 +1,21 @@
+package tamilnadu1.chennai1;
+public class CommonManInChennai implements TrafficRules
+{
+public static void main(String[] args)
+{
+CommonManInChennai i = new CommonManInChennai();
+i.goByDieselVechile();
+i.goByBicycle();
+}
+ public void goByDieselVechile()
+{
+System.out.println("car");
+}
+ public void goByBicycle()
+{
+System.out.println("hero");
+}
+
+
+
+}
