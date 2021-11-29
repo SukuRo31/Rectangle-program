a56a5fda585ca01c340c78b4c7f7981092bacad9
sukumar <suku310598@gmail.com>
2021-11-29 10:55:37 +0530

    scenario

---
+++ SouthIndia.java
@@ -0,0 +1,20 @@
+abstract class SouthIndia extends India
+{
+public void cultivate()
+{
+System.out.println("Rice and Wheat cultivation");
+}
+public void livingStyle()
+{
+System.out.println("Average development");
+}
+SouthIndia(String primeMinister)
+{
+super("modi");
+System.out.println("modi");
+}
+
+
+
+}
+
