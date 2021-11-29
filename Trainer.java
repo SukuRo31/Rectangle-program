0e935f8e452504078591b5e50aa713f16faafa84
sukumar <suku310598@gmail.com>
2021-11-29 10:17:51 +0530

    scenario 1

---
+++ Trainer.java
@@ -0,0 +1,23 @@
+public class Trainer
+{
+String dept = "Java",institute = "Payilagam";
+private int salary = 10000;
+Trainer(String department,String Tech)
+{
+department = department;
+Tech = Tech;
+System.out.println(department);
+System.out.println(Tech);
+
+}
+
+public int getsalary()
+{
+return salary;
+}
+public void training()
+{
+System.out.println("traning");
+}
+//trainerKumar t = new trainerKumar("cse" , "Payilagam");
+}
