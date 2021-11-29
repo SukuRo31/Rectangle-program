3a779585ad2f0d240d1245d6994f4c6ae050e473
sukumar <suku310598@gmail.com>
2021-11-29 10:52:49 +0530

    scenario

---
+++ India.java
@@ -0,0 +1,11 @@
+abstract class India
+{
+static String captial = "New Delhi";
+public India(String primeMinister)
+{
+System.out.println("our primeminister is"+ primeMinister);
+}
+abstract void speakLanguage();
+abstract void eat();
+abstract void dress();
+}
